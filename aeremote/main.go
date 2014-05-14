3215932efdead8dc22428ae1e39d6a0aa7d173c5
ronoaldo@gmail.com <ronoaldo@gmail.com>
2014-05-14 13:46:34 +0000

    Improved aeremote to allow loading and dumping fixtures.

--- aeremote/main.go
+++ aeremote/main.go
@@ -30,19 +30,21 @@ func (s *StringList) Set(value string) error {
 
 // Command line options
 var (
-	cookie string // Json encoded cookie jar.
-	host   string // Hostname to connect to.
-	port   string // Port to connect to.
-	debug  bool   // Enable/disable debug information.
-	kind   string // Kind to export
+	cookie string                // Json encoded cookie jar.
+	host   string                // Hostname to connect to.
+	port   string                // Port to connect to.
+	debug  bool                  // Enable/disable debug information.
+	dump   string                // Kind to export
+	load   = make(StringList, 0) // StringList to load data into.
 )
 
 func init() {
-	flag.StringVar(&cookie, "cookie", "", "A json encoded cookie file")
+	flag.StringVar(&cookie, "cookie", "remoteapi.cookies", "A json encoded cookie file")
 	flag.StringVar(&host, "host", "localhost", "The server to connect")
 	flag.StringVar(&port, "port", "8888", "The port to connect")
 	flag.BoolVar(&debug, "debug", false, "Display debug information")
-	flag.StringVar(&kind, "kind", "", "Kind to export, ignored when loading")
+	flag.StringVar(&dump, "dump", "", "Datastore kind to export, ignored when loading")
+	flag.Var(&load, "load", "Fixture files to import, ignored when dumping")
 }
 
 func main() {
@@ -59,11 +61,25 @@ func main() {
 	}
 
 	switch {
-	case kind != "":
-		log.Print("Exporting entities ...")
-		err = aetools.DumpFixtures(c, os.Stdout, &aetools.DumpOptions{kind, true})
+	case dump != "":
+		log.Printf("Dumping entities of kind %s...\n", dump)
+		err = aetools.DumpFixtures(c, os.Stdout, &aetools.DumpOptions{dump, true})
 		if err != nil {
 			log.Fatal(err)
+		}
+	case len(load) > 0:
+		log.Println("Loading entities ...")
+		for _, f := range load {
+			fd, err := os.Open(f)
+			if err != nil {
+				log.Printf("Error opening %s\n", err.Error())
+				continue
+			}
+			err = aetools.LoadFixtures(c, fd, &aetools.Options{true})
+			if err != nil {
+				log.Printf("Error loading fixture %s: %s\n", f, err.Error())
+			}
+			fd.Close()
 		}
 	default:
 		err = aetools.DumpFixtures(c, os.Stdout, &aetools.DumpOptions{StatKind, true})
